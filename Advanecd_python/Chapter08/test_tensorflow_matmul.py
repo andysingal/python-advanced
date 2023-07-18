51cba780c9b074d9cda20912cb17cafad74e0f08
Ankush Singal <andysingal@gmail.com>
2023-07-18 11:41:25 +0530

    Add files via upload

---
+++ Advanecd_python/Chapter08/test_tensorflow_matmul.py
@@ -0,0 +1,23 @@
+import tensorflow.compat.v1 as tf
+
+tf.disable_v2_behavior()
+import time
+import numpy as np
+
+N = 5000
+
+A_data = np.random.rand(N, N)
+B_data = np.random.rand(N, N)
+
+# Creates a graph.
+
+with tf.device("/gpu:0"):
+    A = tf.placeholder("float32")
+    B = tf.placeholder("float32")
+
+    C = tf.matmul(A, B)
+
+with tf.Session() as sess:
+    start = time.time()
+    sess.run(C, {A: A_data, B: B_data})
+    print("Matrix multiply ({}) took: {}".format(N, time.time() - start))
---
+++ Advanecd_python/Chapter09/example1.py
@@ -0,0 +1,13 @@
+import requests
+
+url = "http://www.google.com"
+
+res = requests.get(url)
+
+print(res.status_code)
+print(res.headers)
+
+with open("google.html", "w") as f:
+    f.write(res.text)
+
+print("Done.")
